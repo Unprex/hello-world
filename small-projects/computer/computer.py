ce96f63360ca70a9b85a0195df33e3afef6b5313
Unprex <bbrouant@sfr.fr>
2018-07-02 20:14:09 +0200

    Reworking the structure to better represent a real computer.

--- small-projects/computer/computer.py
+++ small-projects/computer/computer.py
@@ -8,18 +8,18 @@ Simulation of a computer.
 BITS = 8
 
 
-def bitsToArray(bits):
+def bitsToArray(bits, size=BITS):
     """Translates a byte string to a data array"""
-    array = [False] * BITS
-    for k in range(min(BITS, len(bits))):
+    array = [False] * size
+    for k in range(size):
         array[k] = bits[-1 - k] == "1"
     return array
 
 
-def arrayToBits(array):
+def arrayToBits(array, size=BITS):
     """Translates a data array to a byte string"""
     bits = ""
-    for k in range(BITS):
+    for k in range(size):
         bits += "1" if array[-1 - k] else "0"
     return bits
 
@@ -32,55 +32,99 @@ def arrayToInt(array):
     return integer
 
 
+def fitArray(array, size):
+    """Creates an array of a specific size"""
+    newArray = [False] * size
+    sizeArray = len(array)
+    delta = sizeArray - size
+    for k in range(size):
+        if k + delta >= 0:
+            newArray[k] = array[k + delta]
+    return newArray
+
+
 class Register:
     """Temporarily stores a data array until needed"""
 
-    def __init__(self):
-        self.data = [None] * BITS
+    def __init__(self, inBUS, outBUS, size=BITS):
+        self.data = [False] * size
+        self.inBUS = inBUS
+        self.outBUS = outBUS
+        self.size = size
 
-    def loadData(self, data):
-        self.data = data
+    def registerIn(self):
+        self.data = fitArray(self.inBUS, self.size)
+
+    def registerOut(self):
+        self.outBUS = fitArray(self.data, len(self.outBUS))
+
+
+class ProgramCounter:
+    """Count the number of steps the program has executed"""
+
+    def __init__(self, BUS, size=(BITS // 2)):
+        self.count = [False] * size
+        self.input = Register(BUS, self.data, size)
+        self.output = Register(self.data, BUS, size)
+        self.size = size
+
+    def counterOut(self):
+        self.output.registerIn()  # count -> register
+        self.output.registerOut()  # register -> BUS
+
+    def counterIn(self):
+        self.input.registerIn()  # BUS -> register
+        self.input.registerOut()  # register -> count
+
+    def counterStep(self):
+        carry, k = True, 0
+        while k < self.size and carry:
+            diff = carry == self.count[k]
+            self.count[k], carry = not diff, diff  # XOR, NXOR
+            k += 1
+
+
+class InstructionRegister:  # TODO
+
+    def __init__(self, BUS):
+        self.data = [False] * BITS
+        self.BUS = BUS
+
+    def setAddress(self, address):
+        self.data = address
 
     def getData(self):
         return self.data
 
 
-class ProgramCounter(Register):
-    """Count the number of steps the program has executed"""
-
-    def nextStep(self):
-        carry, k = True, 0
-        while k < BITS and carry:
-            diff = carry == self.data[k]
-            self.data[k], carry = not diff, diff  # XOR, NXOR
-            k += 1
-
-
 class RAM:
     """Storage and recovery of "size" data arrays"""
 
-    def __init__(self, size=2**(BITS // 2)):
-        self.data = [[None] * BITS for k in range(size)]
+    def __init__(self, BUS, size=(BITS // 2)):
+        self.memory = [[False] * BITS for k in range(2**size)]
+        self.address = [False] * size
+        self.data = [False] * BITS
+        self.input = Register(BUS, self.address, size)
+        self.output = Register(self.data, BUS, size)
+        self.size = size
 
-    def setData(self, data, address):  # Address as a data array
-        self.data[arrayToInt(address)] = data
+    def memoryIn(self):
+        self.input.registerIn()  # BUS -> register
+        self.input.registerOut()  # register -> address
 
-    def getData(self, address):
-        return self.data[arrayToInt(address)]
-
-
-class Decoder:  # TODO
-    def __init__(self):
-        pass
+    def RAMout(self):
+        self.output.registerIn()  # data -> register
+        self.output.registerOut()  # register -> BUS
 
 
 class ALU:
     """Arithmetic and logic unit: Adds or subtracts its inputs"""
 
-    def __init__(self, inputA, inputB):
+    def __init__(self, BUS, inputA, inputB):
         # "inputA" and "inputB" are registers
         self.inputA = inputA
         self.inputB = inputB
+        self.BUS = BUS
 
     def getData(self, substract=False):
         dataA = self.inputA.getData()
@@ -97,6 +141,7 @@ class ALU:
 
 if __name__ == "__main__":  # Example code (incomplete)
     # Set-up everything
+    """
     registerA = Register()
     registerB = Register()
     counter = ProgramCounter()
@@ -104,17 +149,25 @@ if __name__ == "__main__":  # Example code (incomplete)
     ram = RAM()
     BUS = [None] * BITS  # Initialise BUS
     counter.loadData(bitsToArray("0000"))  # Set counter to step 0
-    # Initialise RAM with 13 and 6 at address 0 and 1
-    ram.setData(bitsToArray("00001101"), bitsToArray("0000"))
-    ram.setData(bitsToArray("00000110"), bitsToArray("0001"))
+    # Initialise RAM with 13 and 6 at address 14 and 15
+    ram.setData(bitsToArray("00001101"), bitsToArray("1110"))
+    ram.setData(bitsToArray("00000110"), bitsToArray("1111"))
+
+    while True:
+        # CO MI RO (Counter Out -> Memory In -> RAM Out)
+        BUS = ram.getData(counter.getData())
+        # II (Instruction register In)
+        BUS = ram.getData(counter.getData())
+        counter.nextStep()
+
     # Step 0: Writes "13" from RAM to the BUS
-    BUS = ram.getData(bitsToArray("0000"))
+    BUS = ram.getData(bitsToArray("1110"))
     counter.nextStep()
     # Step 1: Load "13" to "registerA"
     registerA.loadData(BUS)  # a = 13
     counter.nextStep()
     # Step 2: Writes "6" from RAM to the BUS
-    BUS = ram.getData(bitsToArray("0001"))
+    BUS = ram.getData(bitsToArray("1111"))
     counter.nextStep()
     # Step 3: Load "6" to "registerB"
     registerB.loadData(BUS)  # b = 6
@@ -125,3 +178,4 @@ if __name__ == "__main__":  # Example code (incomplete)
     # Output
     print(arrayToBits(BUS))  # Prints BUS content
     print(arrayToBits(counter.getData()))  # Prints the number of steps
+    """
