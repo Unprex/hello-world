9f8bf522477ae3fbfde9cbc8568aee5cb5fc4858
Unprex <bbrouant@sfr.fr>
2018-07-04 01:02:43 +0200

    Added comments, TODOs and harmonized READMEs.

--- small-projects/computer/computer.py
+++ small-projects/computer/computer.py
@@ -1,15 +1,18 @@
 """
-Simulation of a computer.
-
 @file computer.py
+@brief Simulation of a computer.
 @author Unprex
+@version 0.1.2
 """
 
 BITS = 8
 
 
 def bitsToArray(bits):
-    """Translates a byte string to a data array"""
+    """Translates a byte string to a data array.
+    @param bits A string of "0"s and "1"s representing a byte.
+    @return An array of booleans with the least significant bit first.
+    """
     size = len(bits)
     array = [False] * size
     for k in range(size):
@@ -18,7 +21,10 @@ def bitsToArray(bits):
 
 
 def arrayToBits(array):
-    """Translates a data array to a byte string"""
+    """Translates a data array to a byte string
+    @param[in] array An array of booleans with the least significant bit first.
+    @return A string of "0"s and "1"s representing a byte.
+    """
     size = len(array)
     bits = ""
     for k in range(size):
@@ -27,7 +33,10 @@ def arrayToBits(array):
 
 
 def arrayToInt(array):
-    """Translates a data array to an integer"""
+    """Translates a data array to an integer.
+    @param[in] array An array of booleans with the least significant bit first.
+    @return The representation in base 10 of the data array.
+    """
     integer = 0
     for k, bit in enumerate(array):
         integer += 2**k if bit else 0
@@ -35,7 +44,11 @@ def arrayToInt(array):
 
 
 def fitArray(array, newArray, default=False):
-    """Fits an array into another one"""
+    """Fits an array into another one.
+    @param[in] array An array of booleans with the least significant bit first.
+    @param[out] newArray The array to put the values of the first in.
+    @param default The value to use if the array to fit is too small.
+    """
     size = len(array)
     for k in range(len(newArray)):
         if k < size:
@@ -45,7 +58,7 @@ def fitArray(array, newArray, default=False):
 
 
 class Register:
-    """Temporarily stores a data array until needed"""
+    """Temporarily stores a data array until needed."""
 
     def __init__(self, inBUS, outBUS, size=BITS):
         self.data = [False] * size
@@ -61,7 +74,7 @@ class Register:
 
 
 class ProgramCounter(Register):
-    """Count the number of steps the program has executed"""
+    """Count the number of steps the program has executed."""
 
     def __init__(self, BUS, size=(BITS // 2)):
         Register.__init__(self, BUS, BUS, size)
@@ -75,6 +88,7 @@ class ProgramCounter(Register):
 
 
 class InstructionRegister(Register):
+    """Receives instructions and executes them."""
 
     def __init__(self, BUS, instr, instrSize,
                  sizeData=BITS, sizeAddress=(BITS // 2)):
@@ -88,9 +102,13 @@ class InstructionRegister(Register):
         fitArray(self.data[:self.sizeAddress], self.outBUS)
 
     def setFonctions(self, fonct):
+        """Sets the functions to use for operations.
+        @param[in] fonct An array of functions corresponding to instructions.
+        """
         self.fonct = fonct
 
     def update(self):
+        """Updates the computer and executes instructions."""
         for k in range(self.instrSize):
             operation = arrayToInt(self.data[-self.sizeAddress:])
             for i in self.instr[8 * operation + k]:
@@ -98,23 +116,27 @@ class InstructionRegister(Register):
 
 
 class RAM(Register):
-    """Storage and recovery of "size" data arrays"""
+    """Storage and recovery of 2**sizeAddress data arrays."""
 
     def __init__(self, BUS, sizeData=BITS, sizeAddress=(BITS // 2)):
         self.memory = [[False] * sizeData for _ in range(2**sizeAddress)]
         self.address = [False] * sizeAddress
         Register.__init__(self, BUS, BUS, sizeData)
 
-    def MemaIn(self):  # Sets address
+    def MemaIn(self):  # Sets memory address.
         fitArray(self.outBUS, self.address)
         self.data = self.memory[arrayToInt(self.address)]
 
-    def setData(self, address, data):  # Sets address
-        self.memory[arrayToInt(address)] = data
+    def setData(self, address, data):
+        """Used to initialize default RAM values.
+        @param[in] address A data array corresponding to the address to set.
+        @param data A byte string corresponding to the value to set.
+        """
+        self.memory[arrayToInt(address)] = bitsToArray(data)
 
 
 class ALU(Register):
-    """Arithmetic and logic unit: Adds or subtracts its inputs"""
+    """Arithmetic and logic unit: Adds or subtracts its inputs."""
 
     def __init__(self, BUS, size=BITS):
         self.A = Register(BUS, BUS, size)
@@ -134,6 +156,8 @@ class ALU(Register):
 
 
 class Clock:
+    """Used to test if the computer is still running."""
+
     def __init__(self, halt=False):
         self.halt = halt
 
@@ -141,17 +165,22 @@ class Clock:
         self.halt = True
 
     def running(self):
+        """Tests if the computer is running.
+        @return True if the computer is running.
+        """
         return not self.halt
 
 
 class Output(Register):
+    """Used to output data to the console."""
+
     def In(self):
         fitArray(self.inBUS, self.data)
         print(arrayToInt(self.data))
 
 
 if __name__ == "__main__":  # Example code
-    # Define a set of instructions (Carefull with in/out priority)
+    # Define a set of instructions (Careful with in/out priority)
     instr = [[]] * 2**8  # 1*CF 4*Op 3*Step
     for k in range(0, 249, 8):  # 00000XXX -> 11111XXX
         instr[k] = [13, 1]  # CO MI
@@ -184,15 +213,16 @@ if __name__ == "__main__":  # Example code
         cnt.Enable, cnt.Out, cnt.In, lambda: None])
 
     # Initialize RAM with addition code
-    ram.setData(bitsToArray("0000"), bitsToArray("00011110"))  # LDA 14
-    ram.setData(bitsToArray("0001"), bitsToArray("00101111"))  # ADD 15
-    ram.setData(bitsToArray("0010"), bitsToArray("11100000"))  # OUT
-    ram.setData(bitsToArray("0011"), bitsToArray("11110000"))  # HLT
+    ram.setData(bitsToArray("0000"), "00011110")  # LDA 14
+    ram.setData(bitsToArray("0001"), "00101111")  # ADD 15
+    ram.setData(bitsToArray("0010"), "11100000")  # OUT
+    ram.setData(bitsToArray("0011"), "11110000")  # HLT
 
-    # Initialize RAM with 13 and 6 at address 14 and 15
-    ram.setData(bitsToArray("1110"), bitsToArray("00001101"))
-    ram.setData(bitsToArray("1111"), bitsToArray("00000110"))
+    # Initialize RAM with numbers to add at address 14 and 15
+    ram.setData(bitsToArray("1110"), "00011100")  # 28
+    ram.setData(bitsToArray("1111"), "00001110")  # 14
 
     # Run computer
     while clk.running():
         ins.update()
+    # Should print 42 = 28 + 14
